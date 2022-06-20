68599bfe320fe2038b0b650c67a3c29e671af357
Harsh Modi <d007modi@gmail.com>
2022-06-21 00:03:19 +0630

    Updated prac_class

--- prac_class.py
+++ prac_class.py
@@ -1,15 +1,14 @@
-class circle():
-
+class Circle:
     pi = 3.14
 
-    def __init__(self, radius):
-
+    def __init__(self, radius=1):
         self.radius = radius
+        self.area = radius ** 2 * Circle.pi
 
     def get_circumference(self):
+        return self.radius * 2 * self.pi
 
-        return self.radius* 2 * self.pi
 
-
-new_circle = circle(30)
-print(new_circle.get_circumference)
+new_circle = Circle(30)
+print(new_circle.get_circumference())
+print(new_circle.area)
